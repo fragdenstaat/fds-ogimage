94abc3cfd60f1260da9466e93c930b775280f4c8
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-07-03 00:08:47 +0200

    Validate http response on fetch

--- api/_lib/fetch.ts
+++ api/_lib/fetch.ts
@@ -4,5 +4,8 @@ import { ParsedRequest } from './types';
 
 export async function getHtml(parsedReq: ParsedRequest) {
   const response = await fetch(parsedReq.url)
+  if (!response.ok) {
+    throw new Error('Could not get html')
+  }
   return await response.text()
 }
