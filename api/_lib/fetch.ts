cb08b0d2b7b1fd3062b124a4ddb4d6d51936d55c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-07-15 10:39:17 +0200

    Add logging to fetch error

--- api/_lib/fetch.ts
+++ api/_lib/fetch.ts
@@ -5,6 +5,8 @@ import { ParsedRequest } from './types';
 export async function getHtml(parsedReq: ParsedRequest) {
   const response = await fetch(parsedReq.url)
   if (!response.ok) {
+    console.error('fetch error for URL', parsedReq.url)
+    console.error('fetch response', response.status)
     throw new Error('Could not get html')
   }
   return await response.text()
