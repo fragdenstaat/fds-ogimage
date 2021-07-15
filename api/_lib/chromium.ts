afbb841b932d6d998a2f25e4f21d5e10e6c3c7bf
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-07-15 09:58:23 +0200

    Wait on page load Github style

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -17,6 +17,29 @@ export async function getScreenshot(html: string, isDev: boolean) {
     const page = await getPage(isDev);
     await page.setViewport({ width: 1200, height: 630 });
     await page.setContent(html);
+
+    // Use Github's waiting on DOM:
+    // https://github.blog/2021-06-22-framework-building-open-graph-images/
+    await page.evaluate(async () => {
+        const selectors = Array.from(document.querySelectorAll("img"));
+        await Promise.all([
+            document.fonts.ready,
+            ...selectors.map((img) => {
+                // Image has already finished loading, let’s see if it worked
+                if (img.complete) {
+                    // Image loaded and has presence
+                    if (img.naturalHeight !== 0) return;
+                    // Image failed, so it has no height
+                    throw new Error("Image failed to load");
+                }
+                // Image hasn’t loaded yet, added an event listener to know when it does
+                return new Promise((resolve, reject) => {
+                    img.addEventListener("load", resolve);
+                    img.addEventListener("error", reject);
+                });
+            }),
+        ]);
+    });
     const file = await page.screenshot({ type: 'png' });
     return file;
 }
